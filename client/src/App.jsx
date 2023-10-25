f0a5a5a1c218543a0d2c693946a7e186d137f689
ferrary7 <ary7sharma@gmail.com>
2023-10-26 02:07:46 +0530

    Login/SignUp added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,16 @@
-
+import { Route, Routes } from 'react-router-dom'
 import LandingPage from './LandingPage'
 import "./LandingPage.css"
+import Login from './Components/Login/Login'
+import SignUp from './Components/SignUp/Signup'
 function App() {
 
   return (
-    <>
-  
-  <LandingPage/>
-    </>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/users/login" element={<Login />} />
+      <Route path="/users/register" element={<SignUp />} />
+    </Routes>
   )
 }
 
