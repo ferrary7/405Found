e91f5f7f6b8c11e57401925801680d86b2ae4c81
Tanishq Patidar <tanishq.patidar@kalvium.community>
2023-10-26 03:38:31 +0530

    Code for below hero section done

---
+++ client/src/Components/BelowHeroSection.jsx
@@ -0,0 +1,195 @@
+import { AiTwotoneStar } from "react-icons/ai";
+
+const BelowHeroSection = () => {
+  return (
+    <div className="below-herosection-cont">
+      <div className="below-hero-box-cont">
+        <div className="star-cont">
+          <AiTwotoneStar color="white" size={24} />
+          <AiTwotoneStar color="white" size={24} />
+          <AiTwotoneStar color="white" size={24} />
+          <AiTwotoneStar color="white" size={24} />
+          <AiTwotoneStar color="#B7B7B8" size={24} />
+        </div>
+
+        <div id="middle-text">
+          <h1>+15K</h1>
+          <p>Product Reviews</p>
+        </div>
+
+        <div id="last-text">
+          <h2 style={{ color: "white" }}>
+            Real identity- <br />
+            verified reviews
+            <br />
+            you can trust
+          </h2>
+        </div>
+      </div>
+
+      <div className="below-hero-small-box-cont">
+        <div className="below-hero-small-box"></div>
+
+        <div className="below-hero-small-box">
+          <h2
+            style={{
+              color: "#FFF",
+              fontFamily: "Red Hat Display",
+              fontWeight: 700,
+            }}
+          >
+            Quality
+            <br />
+            products from
+            <br />
+            local businesses
+          </h2>
+        </div>
+      </div>
+
+      <div
+        className="below-hero-small-box"
+        style={{ backgroundColor: "#F6F6F6" }}
+      >
+        <h1
+          style={{
+            color: "#000",
+            fontFamily: "Red Hat Display",
+            fontSize: "2rem",
+            fontWeight: 700,
+            margin: 0,
+          }}
+        >
+          4,897+
+        </h1>
+        <p
+          style={{
+            color: "#B7B7B8",
+            fontFamily: "Red Hat Display",
+            fontSize: "1rem",
+            fontWeight: 700,
+            margin: 0,
+          }}
+        >
+          Certified Brands
+        </p>
+      </div>
+
+      <div
+        className="below-hero-small-box"
+        style={{ backgroundColor: "#E6E5FF", height: "24rem", gap: "5rem" }}
+      >
+        <div
+          className="inside-small-box-cont"
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <h3
+            style={{
+              color: "#000",
+              fontFamily: "Red Hat Display",
+              fontSize: "2rem",
+              fontWeight: 700,
+              margin: 0,
+            }}
+          >
+            10%+
+          </h3>
+          <p
+            style={{
+              color: "#B7B7B8",
+              textAlign: "center",
+              fontFamily: "Red Hat Display",
+              fontSize: "1rem",
+              fontWeight: 700,
+              margin: 0,
+            }}
+          >
+            Up to 10% back on
+            <br />
+            all purchases
+          </p>
+        </div>
+        <div className="inside-small-box-cont">
+          <p
+            style={{
+              color: "#B7B7B8",
+              textAlign: "center",
+              fontFamily: "Red Hat Display",
+              fontSize: "1rem",
+              fontWeight: 700,
+              margin: 0,
+            }}
+          >
+            Free, fast, and
+            <br />
+            reliable shipping
+          </p>
+        </div>
+      </div>
+
+      <div
+        className="below-hero-box-cont"
+        style={{ backgroundColor: "#2A61DB" }}
+      >
+        <h3
+          style={{
+            color: "#FFF",
+            fontFamily: "Red Hat Display",
+            fontSize: "1.25rem",
+            fontWeight: 600,
+          }}
+        >
+          See how 405 found sellers compare to sites like Amazon, Etsy, and
+          <br />
+          others
+        </h3>
+        <div style={{width: "100%"}}>
+        <hr style={{ width: "100%" }} />
+        <div className="display-row">
+          <p
+            style={{
+              color: "#FFF",
+              fontFamily: "Red Hat Display",
+              fontSize: "1rem",
+              fontWeight: 600,
+              margin: 0
+            }}
+          >
+            Learn more
+          </p>
+          <span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="13"
+              height="13"
+              viewBox="0 0 13 13"
+              fill="none"
+            >
+              <path
+                d="M3.79167 9.20832L9.20834 3.79166"
+                stroke="black"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+              <path
+                d="M3.79167 3.79166H9.20834V9.20832"
+                stroke="black"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+          </span>
+        </div>
+        </div>
+
+      </div>
+    </div>
+  );
+};
+
+export default BelowHeroSection;
